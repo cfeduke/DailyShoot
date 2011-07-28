85128a20d6d34b7c91bf001c29e831384c6adb5e
Charles Feduke <charles.feduke@gmail.com>
2011-07-28 11:37:05 -0400

    Initial Commit

---
+++ DailyShoot/DailyShootAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DailyShootAppDelegate.h
+//  DailyShoot
+//
+//  Created by Charles Feduke on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyShootAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
