9966d485c9729132fd461f38d0454bee482a6559
Charles Feduke <charles.feduke@gmail.com>
2011-07-28 11:44:26 -0400

    initial commit

---
+++ Assignments.h
@@ -0,0 +1,13 @@
+//
+//  Assignments.h
+//  DailyShoot
+//
+//  Created by Charles Feduke on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Assignments : NSObject
+
+@end
---
+++ Assignments.m
@@ -0,0 +1,23 @@
+//
+//  Assignments.m
+//  DailyShoot
+//
+//  Created by Charles Feduke on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Assignments.h"
+
+@implementation Assignments
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
